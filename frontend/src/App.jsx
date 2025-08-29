b5d372d1e82a07d0cff6988f07d9985f33c219dd
Iraklis Kanarkotis <irakliskanbusiness@gmail.com>
2025-08-29 23:39:00 +0300

    Update project files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import CreatePage from "./pages/CreatePage.jsx";
+import NoteDetailed from "./pages/NoteDetailPage.jsx";
+import { toast } from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+       <button onClick={() =>toast.success("success")}>Click me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailed />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoteDetailPage = () => {
+	return (
+		<div>
+			<h2>Note Detail Page</h2>
+			<p>This is where note details will be displayed.</p>
+		</div>
+	);
+};
+
+export default NoteDetailPage;
