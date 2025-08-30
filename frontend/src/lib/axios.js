4f4d264152d1a6ff45774fdd837f9a1e8ac5a7a0
Iraklis Kanarkotis <irakliskanbusiness@gmail.com>
2025-08-30 20:54:12 +0300

    Update axios.js and other changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const BASE_URL=import.meta.env.MODE === "DEVELOPMENT" ? "http://localhost:5000/api" : "/api"
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Update this if your backend runs on a different port or path
+  baseURL: BASE_URL, // Update this if your backend runs on a different port or path
 });
 
 export default api;
