00026a7dce8a147dc09bde15cba5682f24141ce2
Iraklis Kanarkotis <irakliskanbusiness@gmail.com>
2025-08-30 19:08:42 +0300

    Improve CreatePage layout and spacing

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,41 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-//middleware to parse JSON bodies
-app.use(express.json());
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
-
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
-  next();
-});
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("Server is running http://localhost:", PORT);
+    console.log("Server started on PORT:", PORT);
   });
 });
