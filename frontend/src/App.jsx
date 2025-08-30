e5dc2c1ec17b80232e8118c4d770558637f6a1c9
Iraklis Kanarkotis <irakliskanbusiness@gmail.com>
2025-08-30 19:13:47 +0300

    Update axios baseURL to match backend port

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
-import CreatePage from "./pages/CreatePage.jsx";
-import NoteDetailed from "./pages/NoteDetailPage.jsx";
-import { toast } from "react-hot-toast";
+
+import { Route, Routes } from "react-router";
+
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
 
 const App = () => {
   return (
-    <div>
-       <button onClick={() =>toast.success("success")}>Click me</button>
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
-        <Route path="/note/:id" element={<NoteDetailed />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
   );
 };
-
 export default App;
